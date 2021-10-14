4e106aaf5c4a792b51b7745264499e6ac13a2237
namu1005 <3690cm@naver.com>
2021-10-14 16:29:04 +0900

    로그인 라우터 구현

--- routes/member.js
+++ routes/member.js
@@ -17,6 +17,8 @@ router.post("/Regist", function (request, response) {
     conn.query(sql, [mem_id, pw, nick, birth, gender], function (err, rows) {
         if (!err) {
             console.log(rows);
+            
+            
             let arr = new Array();
             let data = new Object();
             data.status = "200";
@@ -32,5 +34,79 @@ router.post("/Regist", function (request, response) {
     //sql 명령 실행
     //conn.end();
 });
+
+// 회원수정 라우터 (by 안영상)
+router.post("/Modify", function (request, response) {
+    console.log(request.body);
+
+    let nick = request.body.nick;
+    let pw = request.body.pw;
+    let birth = request.body.birth;
+    let sex = request.body.sex;
+    let id = "test"; 
+
+    let sql = "update member set nick = ? pw = ? birth = ? sex = ? where id = ?";
+    conn.query(sql, [nick, pw, birth, sex, id], function (err, rows) {
+        if (!err) {
+            console.log(rows);
+            let arr = new Array();
+            let data = new Object();
+            data.status = "200";
+            arr.push(data);
+            let jsonData = JSON.stringify(arr);
+            console.log(jsonData);
+            response.send(jsonData);
+
+        } else {
+            console.log(err);
+        }
+    });
+    //sql 명령 실행
+    //conn.end();
+});
+
+module.exports = router;
+
+//로그인 라우터(by 강성희)
+router.post("/Login", function (request, response) {
+    console.log(request.body);
+
+    let id = request.body.id;
+    let pw = request.body.pw;
+
+    let sql = "select * from likecafe_db where id = ? and pw = ?";
+    conn.query(sql, [id, pw], function (err, rows) {
+        if (!err) {
+
+
+            if (rows.length > 0) {
+                // 성공
+                // 안드로이드한테도 알려줘야징~
+                let arr = new Array();
+                let data = new Object();
+                data.status = "success"; // {"ddong" : "200"}
+                arr.push(data); // [{"ddong" : "200"}]
+                let jsonData = JSON.stringify(arr);
+                console.log(jsonData);
+                response.send(jsonData); // json으로 바꿔서 안드로 ㄱ
+            } else {
+                // 실패
+                let arr = new Array();
+                let data = new Object();
+                data.status = "fail"; // {"ddong" : "200"}
+                arr.push(data); // [{"ddong" : "200"}]
+                let jsonData = JSON.stringify(arr);
+                console.log(jsonData);
+                response.send(jsonData); // json으로 바꿔서 안드로 ㄱ
+            }
+
+        } else {
+            console.log(err);
+        }
+    });
+    //sql 명령 실행
+    //conn.end();
+});
+
 
 module.exports = router;
