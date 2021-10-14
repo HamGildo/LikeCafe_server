316c6eb796c6f60fcd642efa5c7fa6cf79d51077
65646269 <b65646269@gmail.com>
2021-10-14 16:01:07 +0900

    회원정보수정

--- routes/member.js
+++ routes/member.js
@@ -17,6 +17,8 @@ router.post("/Regist", function (request, response) {
     conn.query(sql, [mem_id, pw, nick, birth, gender], function (err, rows) {
         if (!err) {
             console.log(rows);
+            
+            
             let arr = new Array();
             let data = new Object();
             data.status = "200";
@@ -32,5 +34,38 @@ router.post("/Regist", function (request, response) {
     //sql 명령 실행
     //conn.end();
 });
+
+
+// 회원수정 라우터 (by 안영상)
+router.post("/Modify", function (request, response) {
+    console.log(request.body);
+
+    let nick = request.body.nick;
+    let pw = request.body.pw;
+    let birth = request.body.birth;
+    let sex = request.body.sex;
+    let id = "test"; 
+
+    let sql = "update member set nick = ? pw = ? birth = ? sex = ? where id = ?";
+    conn.query(sql, [nick, pw, birth, sex, id], function (err, rows) {
+        if (!err) {
+            console.log(rows);
+            let arr = new Array();
+            let data = new Object();
+            data.status = "200";
+            arr.push(data);
+            let jsonData = JSON.stringify(arr);
+            console.log(jsonData);
+            response.send(jsonData);
+
+        } else {
+            console.log(err);
+        }
+    });
+    //sql 명령 실행
+    //conn.end();
+});
+
+
 
 module.exports = router;
