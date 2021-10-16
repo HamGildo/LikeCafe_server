958a340cd6dc113c153c3cb6e50d2270028284de
65646269 <b65646269@gmail.com>
2021-10-16 11:46:39 +0900

    review

--- routes/review.js
+++ routes/review.js
@@ -3,7 +3,29 @@ const router = express.Router();
 
 const conn = require("../config/DBConn.js");
 
-router.post("/Regist", function (request, response) {
+router.post("/Review", function (request, response) {
+    console.log(request.body);
+
+    let review = request.body.et_review_writebox;
+
+    let sql = "insert into review values(?)";
+    conn.query(sql, [review], function(err, rows){
+        if (!err) {
+            console.log(rows);
+
+            let str = new String();
+            let data = new Object();
+            data.status = "200";
+            arr.push(data);
+            let jsonData = JSON.stringify(str);
+            console.log(jsonData);
+            response.send(jsonData);
+            
+        } else {
+            console.log(err);
+        }
+    });
+
     
 });
 
