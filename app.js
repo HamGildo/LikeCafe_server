cba5003f060e254db7370dc4faf8413ff7183dab
HamGildo <ham5477@naver.com>
2021-10-16 09:25:29 +0900

    리뷰 라우터 추가

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const cafeRouter = require("./routes/cafe");
 const memberRouter = require("./routes/member");
+const reviewRouter = require("./routes/review");
 const bodyparser = require("body-parser");
 
 app.use(bodyparser.urlencoded({extended:false}));
 app.use('/Cafe',cafeRouter);
 app.use('/Member', memberRouter);
+app.use('/Review', reviewRouter);
 app.listen(3003);
 // 3003 -> server, 3307 -> Mysql
---
+++ routes/review.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const conn = require("../config/DBConn.js");
+
+router.post("/Regist", function (request, response) {
+    
+});
+
+
+module.exports = router;
