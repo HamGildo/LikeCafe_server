83ceaf2285da493a013a187a248a7d95ce4f88ab
HamGildo <ham5477@naver.com>
2021-10-19 04:40:32 +0900

    찜 테이블 라우터 추가, 카페관련 찜 정보 라우터 구현

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const app = express();
 const cafeRouter = require("./routes/cafe");
 const memberRouter = require("./routes/member");
 const reviewRouter = require("./routes/review");
+const zzimRouter = require("./routes/zzim");
 const bodyparser = require("body-parser");
 
 app.use(bodyparser.urlencoded({extended:false}));
 app.use('/Cafe',cafeRouter);
 app.use('/Member', memberRouter);
 app.use('/Review', reviewRouter);
+app.use('/Zzim', zzimRouter);
 app.listen(3003);
 // 3003 -> server, 3307 -> Mysql
---
+++ routes/zzim.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const router = express.Router();
+
+const conn = require("../config/DBConn.js");
+
+router.post("/ZzimCnt", function (request, response) {
+    console.log(request.body);
+
+    let cafe_id = request.body.cafe_id;
+    cafe_id = parseInt(cafe_id);
+    let sql = "select * from zzim where cafe_id = ?";
+    console.log(sql);
+    conn.query(sql, [cafe_id], function (err, rows) {
+        if (!err) {
+            console.log(rows);  
+
+            let arr = new Array();
+            let data = new Object();
+
+            data.zzimCnt = rows.length;
+        
+            arr.push(data);
+            let jsonData = JSON.stringify(arr);
+        
+            console.log(jsonData);
+            response.send(jsonData);
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+router.post("/ZzimSel", function (request, response) {
+    console.log(request.body);
+
+    let cafe_id = request.body.cafe_id;
+    cafe_id = parseInt(cafe_id);
+    let mem_id = request.body.mem_id;
+    
+    let sql = "select * from zzim where cafe_id = ? and mem_id = ?";
+    console.log(sql);
+    conn.query(sql, [cafe_id, mem_id], function (err, rows) {
+        if (!err) {
+            console.log(rows);  
+
+            let arr = new Array();
+            let data = new Object();
+
+            data.zzimSel = rows.length;
+        
+            arr.push(data);
+            let jsonData = JSON.stringify(arr);
+        
+            console.log(jsonData);
+            response.send(jsonData);
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+
+module.exports = router;
