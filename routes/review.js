452790ab1f7585b8aa9bbd6893c9f7045a7b559d
65646269 <b65646269@gmail.com>
2021-10-19 21:33:34 +0900

    리뷰삭제라우터

--- routes/review.js
+++ routes/review.js
@@ -38,5 +38,33 @@ router.post("/ReviewPage", function (request, response) {
     
 });
 
+// 리뷰 삭제하는 라우터다능 !
+router.post("/ReviewDele", function (request, response) {
+    console.log(request.body);
+
+    let cafe_id = request.body.cafe_id;
+    cafe_id = parseInt(cafe_id);
+    let mem_id = request.body.mem_id;
+
+    let sql = "delete from review where mem_id = ? and cafe_id = ?";
+    console.log(sql);
+    conn.query(sql, [mem_id, cafe_id], function (err, rows) {
+        if (!err) {
+            console.log(rows);  
+
+            let arr = new Array();
+            let data = new Object();
+            data.status = "200";
+            arr.push(data);
+            let jsonData = JSON.stringify(arr);
+            console.log(jsonData);
+            response.send(jsonData);
+            
+        } else {
+            console.log(err);
+        }
+    });
+});
+
 
 module.exports = router;
