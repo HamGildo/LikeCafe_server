4a0e8268818b3812907b956a70a504c5804addfb
HamGildo <ham5477@naver.com>
2021-10-20 17:01:34 +0900

    내 리뷰 확인 라우터 구현

--- routes/review.js
+++ routes/review.js
@@ -16,10 +16,10 @@ router.post("/ReviewPage", function (request, response) {
     let content = request.body.content;
     let review_image = request.body.review_image;
     let write_date = request.body.write_date;
-    
+
 
     let sql = "insert into review values (?,?,?,?,?,?,?)";
-    conn.query(sql, [review_id, cafe_id, mem_id, star, content, review_image, write_date], function(err, rows){
+    conn.query(sql, [review_id, cafe_id, mem_id, star, content, review_image, write_date], function (err, rows) {
         if (!err) {
             console.log(rows);
             let arr = new Array();
@@ -29,13 +29,58 @@ router.post("/ReviewPage", function (request, response) {
             let jsonData = JSON.stringify(arr);
             console.log(jsonData);
             response.send(jsonData);
-            
+
         } else {
             console.log(err);
         }
     });
 
+
+});
+
+router.post("/SelectByMemId", function (request, response) {
+    console.log(request.body);
+
+    let mem_id = request.body.mem_id;
     
+    let sql = "select * from review r, cafe c where r.cafe_id = c.cafe_id and mem_id = ?"
+    conn.query(sql, [mem_id], function (err, rows) {
+        if (!err) {
+            console.log(rows);
+
+            let arr = new Array();
+            for (let i = 0; i < rows.length; i++) {
+                let data = new Object(); //여러속성을 하나로 묶어주는 Object생성
+
+                // private int id;
+                // private int cafe_id;
+                // private String cafe_name;
+                // private String mem_id;
+                // private double star;
+                // private String content;
+                // private int image;
+
+                data.review_id = rows[i].review_id;
+                data.cafe_id = rows[i].cafe_id;
+                data.cafe_name = rows[i].cafe_name;
+                data.mem_id = rows[i].mem_id;
+                data.star = rows[i].star;
+                data.content = rows[i].content;
+                data.review_image = rows[i].review_image;
+
+                arr.push(data);
+                //묶인 데이터를 배열에 추가
+            }
+            let jsonData = JSON.stringify(arr);
+
+            console.log(jsonData);
+            response.send(jsonData);
+
+        } else {
+            console.log(err);
+        }
+    });
+
 });
 
 
